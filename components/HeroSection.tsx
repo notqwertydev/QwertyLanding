3e87cc1856467edaf8cdc545b1771b2495123084
RRTDKS <ryan.alam001@gmail.com>
2024-02-21 22:21:02 +0200

    new hero section (undeifned error at the end of the line

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,57 +1,127 @@
 "use client";
 // HeroSection.js
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
-import { Meteors } from './magicui/meteors';
-import { Mail, MapPin } from "lucide-react";
+import Typed from 'react-typed';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import Link from 'next/link';
+
 const HeroSection = () => {
+const codeLines = [
+    '  function App() {'+ '\n' +
+    ' const greeting = "Hello, World!";'+ '\n' +
+    ' console.log(greeting);'+ '\n' +
+    ' }'+ '\n' +
+    " const addNumbers = (a, b) => {"+ '\n' +
+    " return a + b;"+ '\n' +
+    " }"+ '\n' +
+    " const result = addNumbers(3, 4);"+ '\n' +
+    ' console.log(result);'];
 
+    const [typedCode, setTypedCode] = useState("");
+    const codePosition = useRef({ line: 0, char: 0 });
 
+    useEffect(() => {
+        const typingInterval = setInterval(() => {
+            if (codePosition.current.line < codeLines.length) {
+                const line = codeLines[codePosition.current.line];
+                if (codePosition.current.char < line.length) {
+                    setTypedCode((prevCode) => prevCode + line[codePosition.current.char]);
+                    codePosition.current.char++;
+                } else {
+                    setTypedCode((prevCode) => prevCode);
+                    codePosition.current.char = 0;
+                    codePosition.current.line++;
+                }
+            } else {
+                clearInterval(typingInterval);
+            }
+        }, 75); // Replace '75' with the desired interval duration in milliseconds
+    
+        return () => clearInterval(typingInterval);
+    }, [codeLines, setTypedCode, codePosition]);
+
+    // Declare codeLines as an array of strings
     return (
-        <section className="lg:p-8 overflow-hidden">
-                    <div className="mx-auto py-24 lg:px-16 md:px-12 px-8 xl:px-40 items-center lg:py-40 w-full bg-gradientup rounded-[3rem] overflow-hidden relative">
-                        <img
-                            className="hidden lg:block lg:absolute mt-6 bottom-8 right-0 mr-2 h-[500px] w-[500px]"
-                            src="/logos/qwertyanimationwhite.gif">
-                        </img>
-                <div className="max-w-xl">
-                    <div className="mt-6">
-                        <h1 className="text-3xl mt-6 tracking-tighter font-light lg:text-6xl text-white">
-                            <Image
-                                className="image-animation"
-                                src="/logos/qwerty.png"
-                                alt=""
-                                width={250}
-                                height={250}
-                            />
-                            <span className="block text-zinc-400"></span>
-                        </h1>
-                        <p className="text-white mt-6 image-animation" >
-                            Bespoke applications, sophisticated software solutions, and interactive websites.
-                        </p>
-                        <p>
-
-                        </p>
-
-                        <div className="inline-flex flex-wrap items-center mt-8">
-                            <Link
-                                href="https://calendly.com/callqwerty/30min"
-                                target="_blank"
-                                rel='noreferrer noopener'
-                                className="text-sm py-2 px-4 border focus:ring-2 rounded-full border-transparent bg-white hover:bg-white/10 text-black duration-200 focus:ring-offset-2 focus:ring-white hover:text-white inline-flex items-center justify-center ring-1 ring-transparent button-animation"
-                            >
-                                Book A Call With Us!
-                            </Link>
+        <section className="lg:p-8 overflow-hidden ">
+            <div className=" rounded-[3rem] relative isolate overflow-hidden">
+                <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-40">
+                    <div className="px-6 lg:px-0 lg:pt-4">
+                        <div className="mx-auto max-w-2xl">
+                            <div className="max-w-lg">
+                                <div className="mt-24 sm:mt-32 lg:mt-16">
+                                </div>
+                                <Image
+                                    className="image-animation"
+                                    src="/logos/qwerty.png"
+                                    alt=""
+                                    width={250}
+                                    height={250}
+                                />
+                                <p className="text-white mt-6 image-animation" >
+                                    Bespoke applications, sophisticated software solutions, and interactive websites.
+                                </p>
+                                <div className="mt-10 flex items-center gap-x-6">
+                                    <Link
+                                        href="https://calendly.com/callqwerty/30min"
+                                        target="_blank"
+                                        rel='noreferrer noopener'
+                                        className="text-sm py-2 px-4 border focus:ring-2 rounded-full border-transparent bg-white hover:bg-white/10 text-black duration-200 focus:ring-offset-2 focus:ring-white hover:text-white inline-flex items-center justify-center ring-1 ring-transparent button-animation"
+                                    >
+                                        Book A Call With Us!
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-
                     </div>
+                    <div className="mt-20  sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
+                        <div
+                            className="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-gradientup  shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36"
+                            aria-hidden="true"
+                        />
+                        <div className=" hidden md:block shadow-lg md:rounded-3xl">
+                            <div className=" [clip-path:inset(0)] md:[clip-path:inset(0_round_theme(borderRadius.3xl))]">
+                                <div
+                                    className="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white md:ml-20 lg:ml-36"
+                                    aria-hidden="true"
+                                />
+                                <div className="relative md:pr-0">
+                                    <div className="mx-auto max-w-2xl md:mx-0 md:max-w-none">
+                                        <div className="w-screen overflow-hidden rounded-tl-xl bg-gray-900">
+                                            <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                                                <div className="-mb-px flex text-sm font-medium leading-6 text-gray-400">
+                                                    <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                                                        MyFirstWebApp.jsx
+                                                    </div>
+                                                    <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
+                                                </div>
+                                            </div>
 
+                                            <div>
+                                                <SyntaxHighlighter language="typescript" style={oneDark}>
+                                                    {typedCode}
+                                                </SyntaxHighlighter>
 
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div
+                                        className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 md:rounded-3xl"
+                                        aria-hidden="true"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t " />
 
 
-                {/* <Meteors number={30} /> */}
+
+
+
             </div >
             <div className="mx-auto   items-center  relative w-full">
                 <div className="relative isolate lg:flex-col overflow-hidden  rounded-[3rem] lg:flex p-3">
@@ -95,7 +165,10 @@ const HeroSection = () => {
                     </div>
 
                 </div>
+
             </div>
+
+
 
 
 
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,15 +1,30 @@
-// components/Footer.js
+// components/Footer.tsx
 
-const Footer = () => {
+import { FaInstagram, FaWhatsapp, FaLinkedin } from 'react-icons/fa';
+
+const navigation = [
+  { name: 'Instagram', icon: FaInstagram, href: 'https://www.instagram.com/notqwerty.co?igsh=MThuaGs0ZnR2Z2pkNQ%3D%3D&utm_source=qr' },
+  { name: 'WhatsApp', icon: FaWhatsapp, href: 'https://wa.me/message/ATF4UTDK33T5D1' },
+  { name: 'LinkedIn', icon: FaLinkedin, href: 'https://www.linkedin.com/company/qwerty-com' },
+];
+
+const Footer: React.FC = () => {
   return (
-    <footer className=" border-t border-white/5 lg:p-8">
-      <div className="flex flex-col justify-between items-center px-8 py-6 mx-auto md:px-12 lg:px-16 xl:px-36 2xl:max-w-7xl">
-        <section className="gap-x-8 gap-y-6 mt-6">
-          <div>
-            <p className="text-sm text-zinc-400 lg:col-span-2">
-              © 2023 notqwerty. All rights reserved.  </p>
-          </div>
-        </section>
+    <footer>
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-xs leading-5 text-gray-500">
+            &copy; 2020 Your Company, Inc. All rights reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
