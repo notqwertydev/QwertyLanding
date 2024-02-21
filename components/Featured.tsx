7cf89f5a86c0cae7fa0a5b5595f89d427ad12488
RRTDKS <ryan.alam001@gmail.com>
2024-02-21 18:46:34 +0200

    Fixed minor things in logos

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -13,36 +13,42 @@ const FeaturesSection = () => {
                                 QWERTY adopts a unique approach to project execution. <br /> We harness the power of the global freelance market to assemble the perfect team for each project.
                             </p>
                         </div>
-                            <div className="mt-12 mx-auto max-w-7xl px-6 lg:px-8">
-                                <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-                                    <img
-                                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                                        src="/logos/radec.png"
-                                        alt="radec"
-                                    />
-                                    <img
-                                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                                        src="/logos/nn.png"
-                                        alt="nn"
-                                    />
-                                    <img
-                                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                                        src="/logos/damco.png"
-                                        alt="damco"
-                                    />
-                                    <img
-                                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-                                        src="/logos/aeolos.png"
-                                        alt="aeolos"
-                                    />
-                                    <img
-                                        className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-                                        src="/logos/kiwi.png"
-                                        alt="kiwi"
-                                    />
+                        <div className="mt-12 mx-auto max-w-7xl px-6 lg:px-8">
+                            <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+                                <img
+                                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                    src="/logos/radec.png"
+                                    alt="radec"
+                                />
+                                <img
+                                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                    src="/logos/nn.png"
+                                    alt="nn"
+                                />
+                                <img
+                                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                    src="/logos/damco.png"
+                                    alt="damco"
+                                />
+                                <img
+                                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                    src="/logos/nazir.png"
+                                    alt="nazir"
+                                />
+                                <img
+                                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                    src="/logos/kiwi.png"
+                                    alt="kiwi"
+                                />
+                                <img
+                                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                    src="/logos/aeolos.png"
+                                    alt="aeolos"
+                                />
 
-                                </div>
+
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
